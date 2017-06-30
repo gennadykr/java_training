cbebb40c844e7bfbbd89a8e7d9f7226db25fa33c
Gennady Krasnikov <gennady.krasnikov@gmail.com>
2017-06-30 09:44:26 +0300

    Задание №5: Реализовать двухуровневую архитектуру тестового набора

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -3,18 +3,17 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class NavigationHelper {
-    private FirefoxDriver wd;
+public class NavigationHelper extends HelperBase {
 
     public NavigationHelper(FirefoxDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void gotoGroupPage() {
-        wd.findElement(By.linkText("groups")).click();
+        click(By.linkText("groups"));
     }
 
     public void gotoContactPage() {
-        wd.findElement(By.id("logo")).click();
+        click(By.id("logo"));
     }
 }
