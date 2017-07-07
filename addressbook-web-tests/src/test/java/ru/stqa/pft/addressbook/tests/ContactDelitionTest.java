1c3b165181b5336cb48eebea60ba0f3f6eb92224
Gennady Krasnikov <gennady.krasnikov@gmail.com>
2017-07-07 06:18:36 +0300

    toString and equals methods added for group and contact models
    Added group and contact list compearing in deletion tests.
    FF find deleted element for contacts (it too fast)! Returned to Chrome
    now.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDelitionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDelitionTest.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
+import java.util.List;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -11,9 +13,13 @@ public class ContactDelitionTest extends TestBase {
         if (!app.getContactHelper().isThereAContact()){
             app.getContactHelper().createContact(new ContactData("test7", null, null, null, null));
         }
+        List<ContactData> before = app.getContactHelper().getContactList();
         app.getContactHelper().selectContact(0);
         app.getContactHelper().deleteSelectedContacts();
         app.getNavigationHelper().gotoContactPage();
+        List<ContactData> after = app.getContactHelper().getContactList();
+        before.remove(0);
+        Assert.assertEquals(before, after);
     }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
+import java.util.List;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -11,9 +13,13 @@ public class GroupDeletionTest extends TestBase {
         if (!app.getGroupHelper().isThereAGroup()){
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
+        List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(0);
         app.getGroupHelper().deleteSelectedGrouops();
         app.getGroupHelper().returnToGroupPage();
+        List <GroupData> after = app.getGroupHelper().getGroupList();
+        before.remove(0);
+        Assert.assertEquals(before, after);
     }
 
 }
