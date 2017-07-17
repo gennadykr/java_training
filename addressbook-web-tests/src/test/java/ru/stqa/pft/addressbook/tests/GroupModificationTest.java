2e702208205296771fabf644e3b346d023c91b36
Gennady Krasnikov <gennady.krasnikov@gmail.com>
2017-07-17 08:12:17 +0300

    Group tests use DB connection for verification
    equals() changed to check all fields of Group object

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -13,18 +13,19 @@ public class GroupModificationTest extends TestBase {
     @BeforeMethod
     public void ensurePreconditions(){
         app.goTo().groupPage();
-        if (app.group().all().size() == 0){
+        if (app.db().groups().size() == 0){
             app.group().create(new GroupData().withName("test1"));
         }
     }
 
     @Test
     public void testGroupModification(){
-        Groups before = app.group().all();
+        Groups before = app.db().groups();
         GroupData modifiedGroup = before.iterator().next();
-        GroupData group = new GroupData().withId(modifiedGroup.getId()).withName("test1").withHeader("test2");
+        GroupData group = new GroupData().withId(modifiedGroup.getId()).withName("test1").withHeader("test2")
+                .withFooter("test 3");
         app.group().modify(group);
-        Groups after = app.group().all();
+        Groups after = app.db().groups();
 
         assertThat(after, equalTo(before.withRemoved(modifiedGroup).withAdded(group)));
     }
