18e1802ab6e6c72e42e8bf7804cae186fbf2c479
Gennady Krasnikov <gennady.krasnikov@gmail.com>
2017-07-19 10:40:48 +0300

    FTP helper and base helper were added

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HelperBase.java
@@ -0,0 +1,52 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class HelperBase {
+    protected final ApplicationManager app;
+    protected WebDriver wd;
+
+    public HelperBase(ApplicationManager app) {
+        this.app = app;
+        wd = app.getDriver();
+    }
+
+    protected void click(By locator) {
+        wd.findElement(locator).click();
+    }
+
+    protected void type(By locator, String text) {
+        if (text != null) {
+            String existingText = readTextFromEditField(locator);
+            if (!text.equals(existingText)) {
+                click(locator);
+                wd.findElement(locator).clear();
+                wd.findElement(locator).sendKeys(text);
+            }
+        }
+    }
+
+    protected String readTextFromEditField(By locator) {
+        return wd.findElement(locator).getAttribute("value");
+    }
+
+    public boolean isAlertPresent() {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    protected boolean isElementPresent(By locator) {
+        List<WebElement> elements = wd.findElements(locator);
+        int size = elements.size();
+        return size > 0;
+    }
+}
