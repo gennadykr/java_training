36afb4a6ec4866d1f92c9869e627ee6f85589eaa
Gennady Krasnikov <gennady.krasnikov@gmail.com>
2017-07-21 00:35:57 +0300

    WIP: SOAP test
    Issue creation was added

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/SoapHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/SoapHelper.java
@@ -1,11 +1,11 @@
 package ru.stqa.pft.mantis.appmanager;
 
-import biz.futureware.mantis.rpc.soap.client.MantisConnectLocator;
-import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
-import biz.futureware.mantis.rpc.soap.client.ProjectData;
+import biz.futureware.mantis.rpc.soap.client.*;
+import ru.stqa.pft.mantis.model.Issue;
 import ru.stqa.pft.mantis.model.Project;
 
 import javax.xml.rpc.ServiceException;
+import java.math.BigInteger;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.rmi.RemoteException;
@@ -22,9 +22,32 @@ public class SoapHelper {
     }
 
     public Set<Project> gitProjects() throws RemoteException, MalformedURLException, ServiceException {
-        MantisConnectPortType mc = new MantisConnectLocator()
-                .getMantisConnectPort(new URL("http://localhost/mantisbt-2.5.1/api/soap/mantisconnect.php"));
-        ProjectData[] projects = mc.mc_projects_get_user_accessible("administrator", "root");
+        MantisConnectPortType mc = getMantisConnect();
+        ProjectData[] projects = mc.mc_projects_get_user_accessible(app.getProperty("web.adminLogin"), app.getProperty("web.adminPassword"));
         return Arrays.asList(projects).stream().map((p) -> new Project().withId(p.getId().intValue()).withName(p.getName())).collect(Collectors.toSet());
+    }
+
+    private MantisConnectPortType getMantisConnect() throws ServiceException, MalformedURLException {
+        return new MantisConnectLocator()
+                    .getMantisConnectPort(new URL(app.getProperty("soap.url")));
+    }
+
+    public Issue addIssue(Issue issue) throws MalformedURLException, ServiceException, RemoteException {
+        String login = app.getProperty("web.adminLogin");
+        String passwd = app.getProperty("web.adminPassword");
+        MantisConnectPortType mc = getMantisConnect();
+        String[] categories = mc.mc_project_get_categories(login, passwd, BigInteger.valueOf(issue.getProject().getId()));
+        IssueData issueData = new IssueData();
+        issueData.setSummary(issue.getSummary());
+        issueData.setDescription(issue.getDescription());
+        issueData.setProject(new ObjectRef(BigInteger.valueOf(issue.getProject().getId()),issue.getProject().getName()));
+        issueData.setCategory(categories[0]);
+        BigInteger issueId = mc.mc_issue_add(login, passwd, issueData);
+        IssueData createdIssueData = mc.mc_issue_get(login, passwd, issueId);
+        return new Issue().withId(createdIssueData.getId().intValue())
+                .withSummary(createdIssueData.getSummary())
+                .withDescription(createdIssueData.getDescription())
+                .withProject(new Project().withId(createdIssueData.getProject().getId().intValue())
+                                          .withName(createdIssueData.getProject().getName()));
     }
 }
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
@@ -1,12 +1,16 @@
 package ru.stqa.pft.mantis.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import ru.stqa.pft.mantis.model.Issue;
 import ru.stqa.pft.mantis.model.Project;
 
 import javax.xml.rpc.ServiceException;
 import java.net.MalformedURLException;
 import java.rmi.RemoteException;
 import java.util.Set;
+
+import static org.testng.Assert.assertEquals;
 
 public class SoapTests extends TestBase {
 
@@ -17,5 +21,16 @@ public class SoapTests extends TestBase {
         for (Project project : projects) {
             System.out.println(project);
         }
+    }
+
+    @Test
+    public void testCreateIssue() throws RemoteException, ServiceException, MalformedURLException {
+        Set<Project> projects = app.soap().gitProjects();
+        Issue issue = new Issue().withSummary("Test issue")
+                .withDescription("Test issue description")
+                .withProject(projects.iterator().next());
+        Issue created = app.soap().addIssue(issue);
+        System.out.println(created);
+        assertEquals(issue.getSummary(), created.getSummary());
     }
 }
